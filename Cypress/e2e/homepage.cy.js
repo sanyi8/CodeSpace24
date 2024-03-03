5ac4cd055e8385c3b391978d8650c923fb20fdd8
sanyi8 <sanyitelo@gmail.com>
2024-03-03 18:05:58 +0000

    cypress test / spec practice with description

---
+++ Cypress/e2e/homepage.cy.js
@@ -0,0 +1,21 @@
+describe('Computing course page test ', () => {     // informative title
+  it('Visit Edinburgh college website and close cookies modal', () => {
+    // visit url
+    cy.visit('https://www.edinburghcollege.ac.uk/')
+
+    // close the cookies modal
+    // assertion: make sure the "close" button is exists and click on it
+
+    // click on the "Courses" link
+    // assertion: make sure the "Courses" button is exists visible and clickable
+
+    // click on "computing" link
+    // assertion: make sure the "computing" button is exists visible and clickable
+
+
+    // check if we are in the "computing" course page
+    // assertion: make sure the current URL is the "computing" page URL
+
+
+  })
+})
