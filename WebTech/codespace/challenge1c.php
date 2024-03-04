32743d9227466686677143c2cb66e3f722fa89c7
sanyi8 <sanyitelo@gmail.com>
2024-03-04 23:24:34 +0000

    age calculator in php

---
+++ WebTech/codespace/challenge1c.php
@@ -0,0 +1,24 @@
+<?php
+
+//Age Calculator
+
+$age = 25;
+$days = $age * 365;
+$hours = $days * 24;
+$mins = $hours * 60;
+
+//Output
+echo "<h1>Welcome to the Age Calculator!</h1>
+<h2>Your age: $age</h2>
+<ul>
+You have been alive for:
+    <li>$days days</li>
+    <li>$hours hours</li>
+    <li>$mins minutes</li>
+
+</ul>
+
+
+";
+
+?>
