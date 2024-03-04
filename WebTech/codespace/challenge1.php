3d55135d8f673b422ae9d5a4b68e0323faf50b81
sanyi8 <sanyitelo@gmail.com>
2024-03-04 22:56:15 +0000

    area calculator php

---
+++ WebTech/codespace/challenge1.php
@@ -0,0 +1,11 @@
+<?php
+
+//create varibles
+//assign values 10, 5
+$width = 10;
+$height = 5;
+//area variable
+$area = $width * $height;
+//calculate area
+echo "Area calculation based on width: $width m  and height: $height m is Area: $area ㎡"
+?>
