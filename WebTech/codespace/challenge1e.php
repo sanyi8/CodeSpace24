20e0302458015d3401a9ccb55b8349957ae5a194
sanyi8 <sanyitelo@gmail.com>
2024-03-05 13:53:00 +0000

    Associative Arrays - Temperature

--- WebTech/codespace/challenge1e.php
+++ WebTech/codespace/challenge1e.php
@@ -1,7 +1,46 @@
-//echo title
-
 <?php
+// Associative Arrays 
 
+// Create and initialise an array variable using a suitable variable name to display the following values for climate in Edinburgh:
 
+//     The “hottest” months in Edinburgh are normally July and August. During summer, the average low temperatures are 52°F (11°C) and average highs are 66°F (19°C)
+    
+//     The coldest months of the year are January and February, with average lows of 33.8°F (1°C) and highs that rarely exceed 44.6°F (7°C).
+    
+// Output
+
+// Average Temperature in Edinburgh
+// Month				High			Low
+// July-Aug			    19 ℃			11 ℃
+// Jan-Feb				7 ℃	       		1 ℃
+
+$summer = "July-Aug";
+$winter = "Jan-Feb";
+$temperature = array("summer_low" => 11,"summer_low" => 19,"winter_low" =>1 , "winter_high" => 7);
+
+echo "<table class=\"table table-condensed\">
+
+<tr>
+<h1>Average Temperature in Edinburgh</h1>
+</tr>
+
+<tr>
+<th>Monthly</th>
+<th>High</th>
+<th>Low</th>
+</tr>
+
+<tr>
+<th>$summer</th>
+<th>" . $temperature['summer_high'] . " ℃</th>
+<th>" . $temperature['summer_low'] . " ℃</th>
+</tr>
+
+<tr>
+<th>"$winter"</th>
+    <th>" . $temperature['winter_high'] . " ℃</th>
+    <th>" . $temperature['winter_low'] . " ℃</th>
+</tr>
+</table>";
 
 ?>
